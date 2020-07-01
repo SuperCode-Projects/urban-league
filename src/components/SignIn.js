c4ded7bf591a35b66d1206cb0a58b841c92bc9ca
Natalie <natalie@super-code.de>
2020-07-01 20:53:32 +0200

    layout SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,1 +1,31 @@
 import React from "react";
+import "./SignIn.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const SignIn = () => {
+  return (
+    <div id="backgroundSignIn">
+      <form id="formSignIn">
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          id="buttonLogIn"
+        >
+          Log In
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
