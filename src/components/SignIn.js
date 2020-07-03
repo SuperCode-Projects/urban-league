1a7c0b8448aeecac53d6c88ff005f2f8d83515d4
Natalie <natalie@super-code.de>
2020-07-03 14:59:54 +0200

    custom style ui-textfields+button

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,19 +3,56 @@ import "./SignIn.css";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+// Customized styling der UI-Textfields und Buttons
+
+const useStyles = makeStyles({
+  password: {
+    height: "2.8em",
+    backgroundColor: "white",
+    borderRadius: "20px",
+    marginTop: "10px",
+    marginBottom: "10px",
+  },
+  name: {
+    height: "2.8em",
+    backgroundColor: "white",
+    borderRadius: "20px",
+  },
+  button: {
+    backgroundColor: "#56CCF2",
+    borderRadius: "20px",
+    height: "2.8em",
+  },
+});
+
+const styleButton = {
+  bgcolor: "#56CCF2",
+};
+
 const SignIn = () => {
+  const classes = useStyles();
   return (
     <div id="backgroundSignIn" className="background">
       <div id="formSignIn">
-        <TextField id="filled-basic" label="Name" variant="filled" />
         <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
+          className={classes.name}
+          id="filled-basic"
+          label="Name"
           variant="filled"
         />
+        <TextField
+          className={classes.password}
+          id="filled-basic"
+          label="Password"
+          variant="filled"
+          type="password"
+        />
+
         <Button
+          {...styleButton}
+          className={classes.button}
           variant="contained"
           color="primary"
           type="submit"
