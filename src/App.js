9407c913b2c0300102768d35859e89b8a09f5df1
sbirhan <sbirhan@gmail.com>
2020-07-04 11:30:44 +0200

    map.js was deleted

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import SignIn from "./components/SignIn";
 import CourtCard from "./components/CourtCard";
 import Home from "./components/Home";
 import CourtDetails from "./components/CourtDetails";
-import Map from "./components/Map";
 
 function App() {
   return (
