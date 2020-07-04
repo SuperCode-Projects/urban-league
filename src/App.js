65e9d4604e11f823e95c552665bffc043efd7d02
Natalie <natalie@super-code.de>
2020-07-04 11:01:03 +0200

    Merge conflict

--- src/App.js
+++ src/App.js
@@ -8,24 +8,16 @@ import SignIn from "./components/SignIn";
 import CourtCard from "./components/CourtCard";
 import Home from "./components/Home";
 import CourtDetails from "./components/CourtDetails";
-import Map from "./components/Map"
+import Map from "./components/Map";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div id="App">
-<<<<<<< HEAD
         {/* <SignIn /> */}
         {/* <CourtCard /> */}
         {/* <Home /> */}
         <CourtDetails />
-=======
-        <SignIn />
-        {<CourtCard />}
-        {<Home />}
-        {<CourtDetails />}
-        {<Map />}
->>>>>>> 1c958412b9e361b225038492aab904d12b685308
       </div>
     </ThemeProvider>
   );
