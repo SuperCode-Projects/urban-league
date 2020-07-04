6808eb933bc1b664b5d7e89795b485f7aa9c0abb
sbirhan <sbirhan@gmail.com>
2020-07-04 10:34:11 +0200

    Map.js added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignIn from "./components/SignIn";
 import CourtCard from "./components/CourtCard";
 import Home from "./components/Home";
 import CourtDetails from "./components/CourtDetails";
+import Map from "./components/Map"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         {<CourtCard />}
         {<Home />}
         {<CourtDetails />}
+        {<Map />}
       </div>
     </ThemeProvider>
   );
